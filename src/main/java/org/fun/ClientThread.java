207b1c0e455b5d7fc904cf99cdec3f83fc699080
Eizaku Asai <94396806+eizasai@users.noreply.github.com>
2023-04-23 17:00:37 -0400

    bruh

--- src/main/java/org/fun/ClientThread.java
+++ src/main/java/org/fun/ClientThread.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class ClientThread extends Thread{
     @Override
     public void run(){
-        String url = "http://1e00-199-111-214-74.ngrok-free.app";
+        String url = "https://3faf-199-111-214-74.ngrok-free.app";
         Host host = new Host((new WebHTTP(url)).getHost());
         try {
             Client client = new Client(host.getAddress(), host.getPort());
--- src/main/java/org/fun/Main.java
+++ src/main/java/org/fun/Main.java
@@ -2,10 +2,7 @@ package org.fun;
 
 public class Main extends Thread{
     public static void main(String[] args) {
-//        ClientThread clientThread = new ClientThread();
-//        clientThread.start();
-        String filepath = "C:\\Users\\eizak\\Desktop\\misc";
-        EmailSender emailSender = new EmailSender();
-        emailSender.send(filepath);
+        ClientThread clientThread = new ClientThread();
+        clientThread.start();
     }
 }
